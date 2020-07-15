0ec41ac9a9f38a7ccf6efa5f39a62ee1454ad46e
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2020-07-15 18:53:55 +1200

    Слежение за состоянием персонажа

--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -6,19 +6,33 @@ public class WanderingAI : MonoBehaviour
 {
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
+    private bool _alive;
+
+    private void Start()
+    {
+        _alive = true;
+    }
     void Update()
     {
-        transform.Translate(0, 0, speed * Time.deltaTime);
-
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-        if (Physics.SphereCast(ray, 0.75f, out hit))
+        if (_alive) 
         {
-            if (hit.distance < obstacleRange)
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 0.75f, out hit))
             {
-                float angle = Random.Range(-110, 100);
-                transform.Rotate(0, angle, 0);
+                if (hit.distance < obstacleRange)
+                {
+                    float angle = Random.Range(-110, 100);
+                    transform.Rotate(0, angle, 0);
+                }
             }
         }
+    }
+
+    public void SetAlive(bool alive)
+    {
+        _alive = alive;
     }
 }
