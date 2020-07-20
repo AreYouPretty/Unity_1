0bffc667089e71d24721ae78743039470afc85e3
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2020-07-20 18:53:33 +1200

    стрельба и столкновение с целью - часть 1

--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WanderingAI : MonoBehaviour
 {
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
     private bool _alive;
@@ -22,7 +24,17 @@ public class WanderingAI : MonoBehaviour
             RaycastHit hit;
             if (Physics.SphereCast(ray, 0.75f, out hit))
             {
-                if (hit.distance < obstacleRange)
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (_fireball==null)
+                    {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < obstacleRange)
                 {
                     float angle = Random.Range(-110, 100);
                     transform.Rotate(0, angle, 0);
