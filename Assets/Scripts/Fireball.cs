5c6cb45ba1ec1a94aebceb1326dbb9808b8e31ed
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2020-07-21 18:24:38 +1200

    стрельба и столкновение с целью - часть 2

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 10.0f;
+    public int damage = 1;
     void Update()
     {
-        
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+        if (player!=null)
+        {
+            Debug.Log("Player hit");
+        }
+        Destroy(this.gameObject);
     }
 }
