ee3b7092e509c8f76992cbe9635db587044cded1
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2020-09-30 18:42:59 +1200

    Программирование невидимого объекта UIController

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Text scoreLabel;
+    void Update()
+    {
+        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+    }
+
+    public void OnOpenSettings()
+    {
+        Debug.Log("open settings");
+    }
+
+    public void OnPointerDown()
+    {
+        Debug.Log("pointer down");
+    }
+}
